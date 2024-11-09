7fdeb8a047d34f66676669d8cfb6e90008b346cf
Jerakovich <jeral122001@gmail.com>
2024-11-08 23:25:41 -0300

    menu de inicio

--- src/Juego.hs
+++ src/Juego.hs
@@ -38,73 +38,73 @@ mostrarCampo canonPos1 canonPos2 mbDisparo1 mbDisparo2 = do
 -- Limpiar la pantalla
 clearScreen :: IO ()
 clearScreen = putStr "\ESC[2J"
--- hola
+
 -- Escuchar en tiempo real para mover los cañones
-escucharEnTiempoReal :: Int -> Int -> IO ()
-escucharEnTiempoReal pos1 pos2 = do
+escucharEnTiempoReal :: Int -> Int -> Int -> Int -> IO ()
+escucharEnTiempoReal pos1 pos2 hp1 hp2 = do
     hSetBuffering stdin NoBuffering
     hSetEcho stdin False
     mostrarCampo pos1 pos2 Nothing Nothing
-    inputLoop pos1 pos2 Nothing Nothing
+    inputLoop pos1 pos2 Nothing Nothing hp1 hp2
 
-inputLoop :: Int -> Int -> Maybe (Int, Int) -> Maybe (Int, Int) -> IO ()
-inputLoop pos1 pos2 mbDisparo1 mbDisparo2 = do
-    threadDelay 100000 -- Controla la velocidad de actualización
-    inputAvailable <- hReady stdin
-    if inputAvailable
-        then do
-            char <- getChar
-            case char of
-                'a' -> actualizarPos1 (max 0 (pos1 - 1)) mbDisparo1 mbDisparo2 -- Mover el primer mortero
-                'd' -> actualizarPos1 (min 40 (pos1 + 1)) mbDisparo1 mbDisparo2 -- Limita posición máxima para el primer mortero
-                '<' -> actualizarPos2 (max 41 (pos2 - 1)) mbDisparo1 mbDisparo2 -- Mover el segundo mortero a la izquierda
-                '>' -> actualizarPos2 (min 80 (pos2 + 1)) mbDisparo1 mbDisparo2 -- Mover el segundo mortero a la derecha
-                'w' -> if mbDisparo1 == Nothing 
-                          then disparar1 pos1 pos2
-                          else inputLoop pos1 pos2 mbDisparo1 mbDisparo2
-                'e' -> if mbDisparo2 == Nothing
-                          then disparar2 pos1 pos2
-                          else inputLoop pos1 pos2 mbDisparo1 mbDisparo2
-                'q' -> putStrLn "Juego terminado."
-                _   -> inputLoop pos1 pos2 mbDisparo1 mbDisparo2
-        else moverDisparo1 pos1 pos2 mbDisparo1 >> moverDisparo2 pos1 pos2 mbDisparo2
-
+inputLoop :: Int -> Int -> Maybe (Int, Int) -> Maybe (Int, Int) -> Int -> Int -> IO ()
+inputLoop pos1 pos2 mbDisparo1 mbDisparo2 hp1 hp2 = do
+    when (hp1 > 0 && hp2 > 0) $ do
+        threadDelay 100000 -- Controla la velocidad de actualización
+        inputAvailable <- hReady stdin
+        if inputAvailable
+            then do
+                char <- getChar
+                case char of
+                    'a' -> actualizarPos1 (max 0 (pos1 - 1)) mbDisparo1 mbDisparo2 hp1 hp2 -- Mover el primer mortero
+                    'd' -> actualizarPos1 (min 40 (pos1 + 1)) mbDisparo1 mbDisparo2 hp1 hp2 -- Limita posición máxima para el primer mortero
+                    '<' -> actualizarPos2 (max 41 (pos2 - 1)) mbDisparo1 mbDisparo2 hp1 hp2 -- Mover el segundo mortero a la izquierda
+                    '>' -> actualizarPos2 (min 80 (pos2 + 1)) mbDisparo1 mbDisparo2 hp1 hp2 -- Mover el segundo mortero a la derecha
+                    'w' -> if mbDisparo1 == Nothing 
+                              then disparar1 pos1 pos2 hp1 hp2
+                              else inputLoop pos1 pos2 mbDisparo1 mbDisparo2 hp1 hp2
+                    'e' -> if mbDisparo2 == Nothing
+                              then disparar2 pos1 pos2 hp1 hp2
+                              else inputLoop pos1 pos2 mbDisparo1 mbDisparo2 hp1 hp2
+                    'q' -> putStrLn "Juego terminado."
+                    _   -> inputLoop pos1 pos2 mbDisparo1 mbDisparo2 hp1 hp2
+            else moverDisparo1 pos1 pos2 mbDisparo1 hp1 hp2 >> moverDisparo2 pos1 pos2 mbDisparo2 hp1 hp2
   where
     -- Actualizar posición del primer mortero
-    actualizarPos1 newPos mbDisparo1 mbDisparo2 = do
+    actualizarPos1 newPos mbDisparo1 mbDisparo2 hp1 hp2 = do
         mostrarCampo newPos pos2 mbDisparo1 mbDisparo2
-        inputLoop newPos pos2 mbDisparo1 mbDisparo2
+        inputLoop newPos pos2 mbDisparo1 mbDisparo2 hp1 hp2
     
     -- Actualizar posición del segundo mortero
-    actualizarPos2 newPos mbDisparo1 mbDisparo2 = do
+    actualizarPos2 newPos mbDisparo1 mbDisparo2 hp1 hp2 = do
         mostrarCampo pos1 newPos mbDisparo1 mbDisparo2
-        inputLoop pos1 newPos mbDisparo1 mbDisparo2
+        inputLoop pos1 newPos mbDisparo1 mbDisparo2 hp1 hp2
 
 -- Función para disparar del primer mortero
-disparar1 :: Int -> Int -> IO ()
-disparar1 canonPos1 canonPos2 = inputLoop canonPos1 canonPos2 (Just (canonPos1 + 4, 18)) Nothing
+disparar1 :: Int -> Int -> Int -> Int -> IO ()
+disparar1 canonPos1 canonPos2 hp1 hp2 = inputLoop canonPos1 canonPos2 (Just (canonPos1 + 4, 18)) Nothing hp1 hp2
 
 -- Función para disparar del segundo mortero
-disparar2 :: Int -> Int -> IO ()
-disparar2 canonPos1 canonPos2 = inputLoop canonPos1 canonPos2 Nothing (Just (canonPos2 + 4, 18))
+disparar2 :: Int -> Int -> Int -> Int -> IO ()
+disparar2 canonPos1 canonPos2 hp1 hp2 = inputLoop canonPos1 canonPos2 Nothing (Just (canonPos2 + 4, 18)) hp1 hp2
 
 -- Mover el disparo en una trayectoria parabólica
-moverDisparo1 :: Int -> Int -> Maybe (Int, Int) -> IO ()
-moverDisparo1 canonPos1 canonPos2 Nothing = inputLoop canonPos1 canonPos2 Nothing Nothing
-moverDisparo1 canonPos1 canonPos2 (Just (x, y))
-    | y <= 0 = inputLoop canonPos1 canonPos2 Nothing Nothing -- El disparo "desaparece" al llegar al suelo
+moverDisparo1 :: Int -> Int -> Maybe (Int, Int) -> Int -> Int -> IO ()
+moverDisparo1 canonPos1 canonPos2 Nothing hp1 hp2 = inputLoop canonPos1 canonPos2 Nothing Nothing hp1 hp2
+moverDisparo1 canonPos1 canonPos2 (Just (x, y)) hp1 hp2
+    | y <= 0 = inputLoop canonPos1 canonPos2 Nothing Nothing hp1 (hp2 - 10) -- El disparo "desaparece" al llegar al suelo y resta HP al oponente
     | otherwise = do
         let newX = x + 1
             newY = max 0 (y - 1)
         mostrarCampo canonPos1 canonPos2 (Just (newX, newY)) Nothing
-        inputLoop canonPos1 canonPos2 (Just (newX, newY)) Nothing
+        inputLoop canonPos1 canonPos2 (Just (newX, newY)) Nothing hp1 hp2
 
-moverDisparo2 :: Int -> Int -> Maybe (Int, Int) -> IO ()
-moverDisparo2 canonPos1 canonPos2 Nothing = inputLoop canonPos1 canonPos2 Nothing Nothing
-moverDisparo2 canonPos1 canonPos2 (Just (x, y))
-    | y <= 0 = inputLoop canonPos1 canonPos2 Nothing Nothing -- El disparo "desaparece" al llegar al suelo
+moverDisparo2 :: Int -> Int -> Maybe (Int, Int) -> Int -> Int -> IO ()
+moverDisparo2 canonPos1 canonPos2 Nothing hp1 hp2 = inputLoop canonPos1 canonPos2 Nothing Nothing hp1 hp2
+moverDisparo2 canonPos1 canonPos2 (Just (x, y)) hp1 hp2
+    | y <= 0 = inputLoop canonPos1 canonPos2 Nothing Nothing (hp1 - 10) hp2 -- El disparo "desaparece" al llegar al suelo y resta HP al oponente
     | otherwise = do
-        let newX = x + 1
+        let newX = x - 1
             newY = max 0 (y - 1)
         mostrarCampo canonPos1 canonPos2 Nothing (Just (newX, newY))
-        inputLoop canonPos1 canonPos2 Nothing (Just (newX, newY))
+        inputLoop canonPos1 canonPos2 Nothing (Just (newX, newY)) hp1 hp2
