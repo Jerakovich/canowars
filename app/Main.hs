7a95e73697062f4ad4c241ba768f9a9886aa7964
Leuis <63936002+Luis-iom@users.noreply.github.com>
2024-11-11 04:32:21 -0300

    Update J+L

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,27 @@
--- app/Main.hs
 import Juego
 import System.IO
 import Text.Read (readMaybe)
-import Control.Monad (when)
+import Control.Monad (when, foldM)
+import Control.Concurrent (threadDelay)
+import System.IO (hFlush, stdout)
+import Data.Fixed (mod')
+import System.Random (randomRIO)
 
 main :: IO ()
 main = do
     putStrLn "\n===== Bienvenido a CANOWARS ====="
     putStrLn "Configura la HP inicial de los morteros (mínimo 10 puntos)."
-
-    putStr "HP de las Catapultas: "
+    
+    putStr "HP del Mortero 1: "
     hFlush stdout
     hp1 <- leerHp
-    let hp2 = hp1
+
+    putStr "HP del Mortero 2: "
+    hFlush stdout
+    hp2 <- leerHp
+
     putStrLn "\nConfiguración completa. Comienza el juego!"
-    inputLoopConHp 20 60 100 100 45 45 hp1 hp2 1
+    inputLoopConHp 20 60 100 100 hp1 hp2 1
 
 -- Función para leer y validar la HP del jugador
 leerHp :: IO Int
@@ -28,121 +35,125 @@ leerHp = do
             leerHp
 
 -- Función que maneja el ciclo del juego por turnos
-inputLoopConHp :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> IO ()
--- pos1: posición del mortero 1
--- pos2: posición del mortero 2
--- fuel1: combustible del mortero 1
--- fuel2: combustible del mortero 2
--- angle1: grado de inclinación del mortero 1
--- angle2: grado de inclinación del mortero 2
--- hp1: HP del mortero 1
--- hp2: HP del mortero 2
--- turno: número de turno actual
-inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 turno = do
+inputLoopConHp :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> IO ()
+inputLoopConHp pos1 pos2 fuel1 fuel2 hp1 hp2 turno = do
     hSetBuffering stdin NoBuffering
     hSetEcho stdin False
-    mostrarCampo pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2
-    
-    -- Si ambos morteros tienen HP positivos, el juego continúa
+    mostrarCampo pos1 pos2 fuel1 fuel2 hp1 hp2
     when (hp1 > 0 && hp2 > 0) $ do
-        -- Mostrar acciones disponibles para el jugador actual
         if turno `mod` 2 == 1
             then do -- Turno del jugador 1
                 putStrLn "\nElige una acción (Jugador 1):"
-                putStrLn "(a: mover izquierda, d: mover derecha, w: elevar cañón, s: bajar cañón, e: disparar)"
-                char1 <- getChar
-                case char1 of
-                    -- Mover mortero 1 izquierda (gasta 5 de combustible, no permite combustible negativo)
-                    'a' -> if fuel1 >= 5
-                        then inputLoopConHp (max 0 (pos1 - 1)) pos2 (max 0 (fuel1 - 5)) fuel2 angle1 angle2 hp1 hp2 turno 
-                        else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-                    
-                    -- Mover mortero 1 derecha (gasta 5 de combustible, no permite combustible negativo)
-                    'd' -> if fuel1 >= 5
-                        then inputLoopConHp (min 40 (pos1 + 1)) pos2 (max 0 (fuel1 - 5)) fuel2 angle1 angle2 hp1 hp2 turno 
-                        else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-
-                    -- Mover cañon hacia arriba (gasta 1 de combustible, no permite combustible negativo)
-                    'w' -> if fuel1 >= 1
-                        then inputLoopConHp pos1 pos2 (max 0 (fuel1 - 1)) fuel2 (min 90 (angle1 + 1)) angle2 hp1 hp2 turno
-                        else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-                    
-                    -- Mover cañon hacia abajo (gasta 1 de combustible, no permite combustible negativo)
-                    's' -> if fuel1 >= 1
-                        then inputLoopConHp pos1 pos2 (max 0 (fuel1 - 1)) fuel2 (max 0 (angle1 - 1)) angle2 hp1 hp2 turno
-                        else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-
-                    -- Disparar mortero 1 (gasta 3 de combustible, no permite combustible negativo)
-                    'e' -> if fuel1 >= 3 
-                        then disparar1Main pos1 pos2 (max 0 (fuel1 - 3)) fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-                        else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-                    
-                    
-                    -- Salir del juego
-                    'p' -> putStrLn "Juego terminado."
-
-                    _   -> putStrLn "Jugador 1. Espera tu turno!"
-                    
-            
+                putStrLn "(a: mover izquierda, d: mover derecha, w: disparar)"
             else do -- Turno del jugador 2
                 putStrLn "\nElige una acción (Jugador 2):"
-                putStrLn "(4: mover izquierda, 6: mover derecha, 7: disparar)"
-                char2 <- getChar
-                case char2 of
-                    -- Mover mortero 2 izquierda (gasta 5 de combustible, no permite combustible negativo)
-                    '4' -> if fuel2 >= 5
-                        then inputLoopConHp pos1 (max 41 (pos2 - 1)) fuel1 (max 0 (fuel2 - 5)) angle1 angle2 hp1 hp2 turno 
-                        else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-                    
-                    -- Mover mortero 2 derecha (gasta 5 de combustible, no permite combustible negativo)
-                    '6' -> if fuel2 >= 5
-                        then inputLoopConHp pos1 (min 81 (pos2 + 1)) fuel1 (max 0 (fuel2 - 5)) angle1 angle2 hp1 hp2 turno 
-                        else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-                    -- Mover cañon hacia arriba (gasta 1 de combustible, no permite combustible negativo)
-                    '8' -> if fuel2 >= 1
-                        then inputLoopConHp pos1 pos2 fuel1 (max 0 (fuel2 - 1)) angle1 (min 90 (angle2 + 1)) hp1 hp2 turno
-                        else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-                    
-                    -- Mover cañon hacia abajo (gasta 1 de combustible, no permite combustible negativo)
-                    '5' -> if fuel2 >= 1
-                        then inputLoopConHp pos1 pos2 fuel1 (max 0 (fuel2 - 1)) angle1 (max 0 (angle2 - 1)) hp1 hp2 turno
-                        else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-
-                    -- Disparar mortero 2 (gasta 3 de combustible, no permite combustible negativo)
-                    '7' -> if fuel2 >= 3 
-                        then disparar2Main pos1 pos2 fuel1 (max 0 (fuel2 - 3)) angle1 angle2 hp1 hp2 (turno + 1)
-                        else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 (turno + 1)
-                    
-                    -- Salir del juego
-                    'p' -> putStrLn "Juego terminado."
-
-                    _   -> putStrLn "Jugador 2. Espera tu turno!"
-
+                putStrLn "(<: mover izquierda, >: mover derecha, e: disparar)"
         
-        putStrLn "(p: salir)"
+        putStrLn "(q: salir)"
+        char <- getChar
+        case char of
+            'a' -> inputLoopConHp (max 0 (pos1 - 1)) pos2 fuel1 fuel2 hp1 hp2 (turno + 1) -- Mover mortero 1
+            'd' -> inputLoopConHp (min 40 (pos1 + 1)) pos2 fuel1 fuel2 hp1 hp2 (turno + 1) -- Mover mortero 1
+            '<' -> inputLoopConHp pos1 (max 41 (pos2 - 1)) fuel1 fuel2 hp1 hp2 (turno + 1) -- Mover mortero 2
+            '>' -> inputLoopConHp pos1 (min 80 (pos2 + 1)) fuel1 fuel2 hp1 hp2 (turno + 1) -- Mover mortero 2
+            'w' -> if fuel1 >= 3 && turno `mod` 2 == 1 then disparar1Main pos1 pos2 fuel1 fuel2 hp1 hp2 (turno + 1) else inputLoopConHp pos1 pos2 fuel1 fuel2 hp1 hp2 (turno + 1)
+            'e' -> if fuel2 >= 3 && turno `mod` 2 == 0 then disparar2Main pos1 pos2 fuel1 fuel2 hp1 hp2 (turno + 1) else inputLoopConHp pos1 pos2 fuel1 fuel2 hp1 hp2 (turno + 1)
+            'q' -> putStrLn "Juego terminado."
+            _   -> inputLoopConHp pos1 pos2 fuel1 fuel2 hp1 hp2 (turno + 1)
 
-        -- Llamada recursiva para el siguiente turno, alternando entre los jugadores
-        if fuel1 == 0 || fuel1 < 3
-            then inputLoopConHp pos1 pos2 100 fuel2 angle1 angle2 hp1 hp2 0
-            else if fuel2 == 0 || fuel2 < 3
-                then inputLoopConHp pos1 pos2 fuel1 100 angle1 angle2 hp1 hp2 1
-                else inputLoopConHp pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 turno
-        
+-- Función para leer y validar el ángulo de disparo
+leerAngulo :: IO Double
+leerAngulo = do
+    putStr "Ingresa el ángulo de disparo (0-90 grados): "
+    hFlush stdout
+    input <- getLine
+    let maybeAngulo = readMaybe input :: Maybe Double
+    case maybeAngulo of
+        Just angulo | angulo >= 0 && angulo <= 90 -> return angulo
+        _ -> do
+            putStrLn "El ángulo debe ser un número entre 0 y 90. Intenta nuevamente."
+            leerAngulo
 
- 
-
-
-
-
--- Función para disparar el primer mortero
-disparar1Main :: Int -> Int -> Int -> Int -> Int -> Int-> Int -> Int -> Int -> IO ()
-disparar1Main pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 turno = do
+disparar1Main :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> IO ()
+disparar1Main pos1 pos2 fuel1 fuel2 hp1 hp2 turno = do
     let newFuel1 = fuel1 - 3
-    let newHp2 = if pos1 == pos2 then hp2 - 10 else hp2
-    inputLoopConHp pos1 pos2 newFuel1 fuel2 angle1 angle2 hp1 newHp2 turno
+    putStrLn "\nJugador 1 dispara..."
+    angulo <- leerAngulo
+    (hit, newHp2) <- animarDisparoParabolico pos1 pos2 pos1 pos2 hp2 angulo
+    if hit
+        then putStrLn "¡Impacto! Jugador 2 recibe daño."
+        else putStrLn "¡Fallaste!"
+    threadDelay 1000000  -- Pausa para mostrar el resultado
+    inputLoopConHp pos1 pos2 newFuel1 fuel2 hp1 newHp2 turno
 
--- Función para disparar el segundo mortero
-disparar2Main :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int-> Int -> IO ()
-disparar2Main pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 turno = do
-    let newHp1 = if pos1 == pos2 then hp1 - 10 else hp1
-    inputLoopConHp pos1 pos2 100 100 angle1 angle2 newHp1 hp2 turno
+disparar2Main :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> IO ()
+disparar2Main pos1 pos2 fuel1 fuel2 hp1 hp2 turno = do
+    let newFuel2 = fuel2 - 3
+    putStrLn "\nJugador 2 dispara..."
+    angulo <- leerAngulo
+    (hit, newHp1) <- animarDisparoParabolico pos2 pos1 pos1 pos2 hp1 angulo
+    if hit
+        then putStrLn "¡Impacto! Jugador 1 recibe daño."
+        else putStrLn "¡Fallaste!"
+    threadDelay 1000000  -- Pausa para mostrar el resultado
+    inputLoopConHp pos1 pos2 fuel1 newFuel2 newHp1 hp2 turno
+
+
+
+-- Función para animar un disparo parabólico con ángulo
+calcularDanio :: IO Int
+calcularDanio = do
+    critico <- randomRIO (1, 100) :: IO Int
+    if critico <= 5
+        then randomRIO (7, 9)  -- Daño crítico
+        else randomRIO (1, 3)  -- Daño normal
+
+animarDisparoParabolico :: Int -> Int -> Int -> Int -> Int -> Double -> IO (Bool, Int)
+animarDisparoParabolico posInicial posObjetivo pos1 pos2 hpObjetivo angulo = do
+    let distancia = abs (posObjetivo - posInicial)
+        direccion = if posObjetivo > posInicial then 1 else -1
+        pasos = [0..distancia]
+        radianes = angulo * pi / 180
+        velocidadInicial = 20.0  -- Ajusta este valor según sea necesario
+        xObjetivo = posObjetivo
+        yObjetivo = 9  -- Ajustar según la altura del objetivo
+    hit <- foldM (\hit paso -> do
+        let t = fromIntegral paso / fromIntegral (length pasos)
+            x = posInicial + round (t * fromIntegral distancia * cos radianes) * direccion
+            y = round $ t * velocidadInicial * sin radianes - 0.5 * 9.8 * t^2
+        clearScreen
+        mostrarCampoConBala x y pos1 pos2 -- Muestra la bala en la posición (x, y)
+        threadDelay 100000                 -- Pausa para la animación
+        return (hit || (x == xObjetivo && y == yObjetivo))
+        ) False pasos
+    clearScreen
+    if hit
+        then do
+            danio <- calcularDanio
+            let newHpObjetivo = hpObjetivo - danio
+            putStrLn $ "¡Impacto! El objetivo recibe " ++ show danio ++ " puntos de daño."
+            return (hit, newHpObjetivo)
+        else return (hit, hpObjetivo)
+
+
+
+
+-- Función para mostrar el campo con la bala
+mostrarCampoConBala :: Int -> Int -> Int -> Int -> IO ()
+mostrarCampoConBala x y pos1 pos2 = do
+    putStrLn "Jugador 1 - HP: 100 | Combustible: 100"
+    putStrLn "Jugador 2 - HP: 100 | Combustible: 100"
+    putStrLn "___________________________________________________________________________________________________"
+    let alturaCampo = 10
+    mapM_ (mostrarLineaConBala x y pos1 pos2) [0..alturaCampo]
+    putStrLn "|_________________________________________________________________________________________________|"
+
+-- Muestra una línea del campo con la bala
+mostrarLineaConBala :: Int -> Int -> Int -> Int -> Int -> IO ()
+mostrarLineaConBala x y pos1 pos2 fila
+    | fila == y = putStrLn $ "|" ++ replicate x ' ' ++ "*" ++ replicate (97 - x) ' ' ++ "|"
+    | fila == 9 = putStrLn $ "|" ++ replicate pos1 ' ' ++ "0-¨°°¨-0" ++ replicate (pos2 - pos1 - 8) ' ' ++ "0-¨°°¨-0" ++ replicate (97 - pos2 - 8) ' ' ++ "|"
+    | otherwise = putStrLn $ "|" ++ replicate 97 ' ' ++ "|"
+
+simularDisparo :: Int -> Int -> Bool
+simularDisparo posDisparo posObjetivo = posDisparo == posObjetivo
--- src/Juego.hs
+++ src/Juego.hs
@@ -5,136 +5,37 @@ import System.IO
 import Control.Concurrent
 
 -- Mostrar el campo de batalla con los cañones y las barras de HP y combustible
-mostrarCampo :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> IO ()
-mostrarCampo pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 = do
+mostrarCampo :: Int -> Int -> Int -> Int -> Int -> Int -> IO ()
+mostrarCampo pos1 pos2 fuel1 fuel2 hp1 hp2 = do
     clearScreen
     -- Mostrar la barra de HP y combustible antes de las acciones
-    putStrLn $ "Jugador 1 - HP: " ++ show hp1 ++ " | Combustible: " ++ show fuel1 ++ " | Ángulo: " ++ show angle1 ++ "°"
-    putStrLn $ "Jugador 2 - HP: " ++ show hp2 ++ " | Combustible: " ++ show fuel2 ++ " | Ángulo: " ++ show angle2 ++ "°"
+    putStrLn $ "Jugador 1 - HP: " ++ show hp1 ++ " | Combustible: " ++ show fuel1
+    putStrLn $ "Jugador 2 - HP: " ++ show hp2 ++ " | Combustible: " ++ show fuel2
     putStrLn ""
     let alturaCampo = 10
     -- Mostrar la separación con asteriscos y el perímetro del tablero
-    putStrLn "_____________________________________________________________________________________________________"
+    putStrLn "___________________________________________________________________________________________________"
     -- Mostrar la visualización del campo de batalla (proyectiles, morteros)
-    mapM_ (mostrarLinea pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2) [0..alturaCampo]
-    putStrLn "|###################################################################################################|"
+    mapM_ (mostrarLinea pos1 pos2 fuel1 fuel2 hp1 hp2) [0..alturaCampo]
+    putStrLn "|_________________________________________________________________________________________________|"
 
 -- Mostrar cada línea del campo de batalla
-mostrarLinea :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> IO ()
-mostrarLinea pos1 pos2 fuel1 fuel2 angle1 angle2 hp1 hp2 fila
-    
+mostrarLinea :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> IO ()
+mostrarLinea pos1 pos2 fuel1 fuel2 hp1 hp2 fila
     | fila == 9 = do
         -- Línea de cañones en la parte más baja
-        putStrLn $ "|" ++ replicate pos1 ' ' ++ "0-¨°°¨-0" ++ replicate (40 - pos1) ' ' ++ "***" ++ replicate (pos2 - 41) ' ' ++ "0-¨°°¨-0" ++ replicate (81-pos2) ' ' ++ "|"
-    | fila == 8 =
-        if angle1 == 0 && angle2 == 0 then -- Listo
-            putStrLn $ "|" ++ replicate (pos1 + 3) ' ' ++ "____"  ++ replicate (41 - pos1) ' ' ++ "***" ++ replicate (pos2 - 40) ' ' ++ "____" ++ replicate (84-pos2) ' ' ++ "|"
-        
-        else if angle1 == 90 && angle2 == 90 then --Listo
-            putStrLn $ "|" ++ replicate (pos1 + 2) ' ' ++ " |"  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ " |" ++ replicate (83-pos2) ' ' ++ "|"
-        
-        else if angle1 == 0 && angle2 == 90 then --
-            putStrLn $ "|" ++ replicate (pos1 + 3) ' ' ++ "____"  ++ replicate (41 - pos1) ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ " |" ++ replicate (83-pos2) ' ' ++ "|"
-        
-        else if angle1 == 90 && angle2 == 0 then -- 
-            putStrLn $ "|" ++ replicate (pos1 + 2) ' ' ++ " |"  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate (pos2 - 40) ' ' ++ "____" ++ replicate (84-pos2) ' ' ++ "|"
-
-        else if angle1 == 0 then --Listo
-            putStrLn $ "|" ++ replicate (pos1 + 3) ' ' ++ "____"  ++ replicate (41 - pos1) ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ combineAngle2 angle2 ++ replicate (84-pos2) ' ' ++ "|"
-       
-        else if angle2 == 0 then -- Listo
-            putStrLn $ "|" ++ replicate (pos1 + 3) ' ' ++ combineAngle1 angle1  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate (pos2 - 40) ' ' ++ "____" ++ replicate (84-pos2) ' ' ++ "|"
-
-        else if angle1 == 90 then -- Listo
-           putStrLn $ "|" ++ replicate (pos1 + 2) ' ' ++ " |"  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ combineAngle2 angle2 ++ replicate (84-pos2) ' ' ++ "|"
-       
-       else if angle2 == 90 then -- Listo
-           putStrLn $ "|" ++ replicate (pos1 + 3) ' ' ++ combineAngle1 angle1  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ " |" ++ replicate (83-pos2) ' ' ++ "|"
-
-        else  
-            putStrLn $ "|" ++ replicate (pos1 + 3) ' ' ++ combineAngle1 angle1  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ combineAngle2 angle2 ++ replicate (84-pos2) ' ' ++ "|"
-            
-    | fila == 7 = 
-        if angle1 == 0 && angle2 == 0 then -- Listo
-            putStrLn $ "|" ++ replicate 48 ' ' ++ "***" ++ replicate 48 ' ' ++"|"
-        
-        else if angle1 == 90 && angle2 == 90 then --Listo
-            putStrLn $ "|" ++ replicate (pos1 + 2) ' ' ++ " |"  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ " |" ++ replicate (83-pos2) ' ' ++ "|"
-        
-        else if angle1 == 0 && angle2 == 90 then -- Listo
-            putStrLn $ "|" ++ replicate 48 ' '  ++ "***" ++ replicate (pos2 - 37) ' ' ++ " |" ++ replicate (83-pos2) ' ' ++ "|"
-
-        else if angle1 == 90 && angle2 == 0 then -- Listo
-            putStrLn $ "|" ++ replicate (pos1 + 2) ' ' ++ " |"  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate 48 ' ' ++ "|"
-
-        else if angle1 == 0 then --
-            putStrLn $ "|" ++  replicate 48 ' ' ++ "***" ++ replicate (pos2 - 38) ' ' ++ combineAngle2 angle2 ++ replicate (85-pos2) ' ' ++ "|"
-    
-        else if angle2 == 0 then -- Listo
-            putStrLn $ "|" ++ replicate (pos1 + 4) ' ' ++ combineAngle1 angle1  ++ replicate (43 - pos1) ' ' ++ "***" ++ replicate 48 ' ' ++ "|"
-
-        else if angle1 == 90 then -- 
-            putStrLn $ "|" ++ replicate (pos1 + 2) ' ' ++ " |"  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate (pos2 - 38) ' ' ++ combineAngle2 angle2 ++ replicate (85-pos2) ' ' ++ "|"
-    
-         else if angle2 == 90 then -- 
-            putStrLn $ "|" ++ replicate (pos1 + 4) ' ' ++ combineAngle1 angle1  ++ replicate (43 - pos1) ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ " |" ++ replicate (83-pos2) ' ' ++ "|"
-
-        else  
-            putStrLn $ "|" ++ replicate (pos1 + 4) ' ' ++ combineAngle1 angle1  ++ replicate (43 - pos1) ' ' ++ "***" ++ replicate (pos2 - 38) ' ' ++ combineAngle2 angle2 ++ replicate (85-pos2) ' ' ++ "|"
-
-    | fila == 6 = 
-        if angle1 == 0 && angle2 == 0 then -- Listo
-            putStrLn $ "|" ++ replicate 48 ' ' ++ "***" ++ replicate 48 ' ' ++"|"
-        
-        else if angle1 == 90 && angle2 == 90 then --Listo
-            putStrLn $ "|" ++ replicate (pos1 + 2) ' ' ++ " |"  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ " |" ++ replicate (83-pos2) ' ' ++ "|"
-        
-        else if angle1 == 0 && angle2 == 90 then -- Listo
-            putStrLn $ "|" ++ replicate 48 ' '  ++ "***" ++ replicate (pos2 - 37) ' ' ++ " |" ++ replicate (83-pos2) ' ' ++ "|"
-
-        else if angle1 == 90 && angle2 == 0 then -- Listo
-            putStrLn $ "|" ++ replicate (pos1 + 2) ' ' ++ " |"  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate 48 ' ' ++ "|"
-        
-        else if angle1 == 90 && angle2 < 45  then -- 
-            putStrLn $ "|" ++ replicate (pos1 + 2) ' ' ++ " |"  ++ replicate (44 - pos1) ' ' ++ "***" ++ replicate 48 ' ' ++ "|"
-
-        else if angle2 == 90  && angle1 < 45 then -- 
-            putStrLn $ "|" ++ replicate 48 ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ " |" ++ replicate (83-pos2) ' ' ++ "|"
-
-        else if angle2 == 90 then
-            putStrLn $ "|" ++ replicate (pos1 + 5) ' ' ++ combineAngle1 angle1  ++ replicate (42 - pos1) ' ' ++ "***" ++ replicate (pos2 - 37) ' ' ++ " |" ++ replicate (83-pos2) ' ' ++ "|"
-        
-        else if angle1 > 45 && angle2 > 45 then --Listo
-            putStrLn $ "|" ++ replicate (pos1 + 5) ' ' ++ "/"  ++ replicate (42 - pos1) ' ' ++ "***" ++ replicate (pos2 - 39) ' ' ++ "\\" ++ replicate (86-pos2) ' ' ++ "|"
-        else if angle1 > 45 then
-            putStrLn $ "|" ++ replicate (pos1 + 5) ' ' ++ "/"  ++ replicate (42 - pos1) ' ' ++ "***" ++ replicate 48 ' ' ++ "|"
-        else if angle2 > 45 then
-            putStrLn $ "|" ++ replicate 48 ' ' ++ "***" ++ replicate (pos2 - 39) ' ' ++ combineAngle2 angle2 ++ replicate (86-pos2) ' ' ++ "|"
-        else
-            putStrLn $ "|" ++ replicate 48 ' ' ++ "***" ++ replicate 48 ' ' ++"|"
-
-    | fila == 5 = putStrLn $ "|" ++ replicate 48 ' ' ++ "   " ++ replicate 48 ' ' ++"|"
-    | fila == 4 = putStrLn $ "|" ++ replicate 48 ' ' ++ "   " ++ replicate 48 ' ' ++"|"
-    | fila == 3 = putStrLn $ "|" ++ replicate 48 ' ' ++ "   " ++ replicate 48 ' ' ++"|"
-    | fila == 2 = putStrLn $ "|" ++ replicate 48 ' ' ++ "   " ++ replicate 48 ' ' ++"|"
-    | fila == 1 = putStrLn $ "|" ++ replicate 48 ' ' ++ "   " ++ replicate 48 ' ' ++"|"
-    | fila == 0 = putStrLn $ "|" ++ replicate 48 ' ' ++ "   " ++ replicate 48 ' ' ++"|"
+        putStrLn $ "|" ++ replicate pos1 ' ' ++ "0-¨°°¨-0" ++ replicate (40 - pos1) ' ' ++ "***" ++ replicate (pos2 - 42) ' ' ++ "0-¨°°¨-0" ++ replicate (80-pos2) ' ' ++ "|"
+    | fila == 8 = putStrLn $ "|" ++ replicate 97 ' ' ++ "|"
+    | fila == 7 = putStrLn $ "|" ++ replicate 97 ' ' ++ "|"
+    | fila == 6 = putStrLn $ "|" ++ replicate 97 ' ' ++ "|"
+    | fila == 5 = putStrLn $ "|" ++ replicate 97 ' ' ++ "|"
+    | fila == 4 = putStrLn $ "|" ++ replicate 97 ' ' ++ "|"
+    | fila == 3 = putStrLn $ "|" ++ replicate 97 ' ' ++ "|"
+    | fila == 2 = putStrLn $ "|" ++ replicate 97 ' ' ++ "|"
+    | fila == 1 = putStrLn $ "|" ++ replicate 97 ' ' ++ "|"
+    | fila == 0 = putStrLn $ "|" ++ replicate 97 ' ' ++ "|"
     | otherwise = return ()
 
 -- Limpiar la pantalla
 clearScreen :: IO ()
-clearScreen = putStr "\ESC[2J\ESC[H"
-
-combineAngle2 :: Int -> String
-combineAngle2 angle2
-    | angle2 < 45 && angle2 > 0 = "\\"
-    | angle2 >= 45 && angle2 < 90 = "\\"
-    | angle2 == 90 = "|"
-    | otherwise = ""
-
-combineAngle1 :: Int -> String
-combineAngle1 angle1
-    | angle1 < 45 && angle1 > 0 = "/"
-    | angle1 >= 45 && angle1 < 90 = "/"
-    | angle1 == 90 = "|"
-    | otherwise = ""
+clearScreen = putStr "\ESC[2J"
